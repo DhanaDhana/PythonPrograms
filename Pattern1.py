04bcd1a8b5f4f4cc0456a391e71544ca61fcd203
DhanaDhana <65442555+DhanaDhana@users.noreply.github.com>
2021-04-13 01:19:36 +0530

    Update Pattern1.py

--- Pattern1.py
+++ Pattern1.py
@@ -1,2 +1,11 @@
-for i in range(10):
-  print("Vaishnavi@Dhana@Sandhya")
+n=int(input("Enter a number:")) 
+for a in range(1,n+1,2):
+    
+    for i in range(1,n+1): 
+        print(" "*(2*n-i-a),end="") 
+        for j in range(1,i+a): 
+            print("*",end=" ") 
+        print() 
+for b in range(1,n+1): 
+    print(" "*(n-2),end="") 
+    print("* "*3) 
